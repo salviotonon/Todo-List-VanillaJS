2106cce2cd09456c7d78dae8c978d8575f52a555
salviotonon <salvio.tonon@gmail.com>
2023-02-06 13:19:20 -0300

    create remove buton, search button

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
+const inputAddTodo = document.querySelector(".form-add-todo input");
+const formTodo = document.querySelector(".form-add-todo");
+const todosContainer = document.querySelector(".todos-container");
+const formSearchTodo = document.querySelector(".form-search");
 
+formTodo.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const inputValue = event.target.add.value.trim();
+
+  if (inputValue) {
+    todosContainer.innerHTML += `
+          <li class="list-group-item d-flex justify-content-between align-items-center">
+              <span>${inputValue}</span>
+              <i class="far fa-trash-alt delete"></i>
+          </li>
+        `;
+    event.target.reset();
+  }
+});
+
+formSearchTodo.addEventListener("input", (event) => {
+  const inputValue = event.target.value.trim().toLowerCase();
+  Array.from(todosContainer.children)
+    .filter((todo) => !todo.textContent.toLowerCase().includes(inputValue))
+    .forEach((todo) => {
+      todo.classList.remove("d-flex");
+      todo.classList.add("hidden");
+    });
+  Array.from(todosContainer.children)
+    .filter((todo) => todo.textContent.toLowerCase().includes(inputValue))
+    .forEach((todo) => {
+      todo.classList.remove("hidden");
+      todo.classList.add("d-flex");
+    });
+});
+
+todosContainer.addEventListener("click", (event) => {
+  if (Array.from(event.target.classList).includes("delete")) {
+    event.target.parentElement.remove();
+  }
+});
